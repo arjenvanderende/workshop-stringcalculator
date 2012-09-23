1001d7eb411bcf9032ac8590a97116a38bada5d0
Arjen van der Ende <git@arjenvanderende.nl>
2012-09-23 12:36:09 +0200

    Ported Java implementation to C#

---
+++ StringCalculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Pyroxene.Kata.StringCalculator
+{
+	public class StringCalculator
+	{
+		public int Add(string input) {
+			return Add(new NumberedString(input));
+		}
+
+		private int Add(NumberedString input) {
+			NumberedString replacedInput = input.ReturnNewInstanceWithNewLinesReplacedByDelimiter();
+			string[] split = replacedInput.GetNumbersAsString();
+			if (HasOneArgument(split)) {
+				return ParseNumber(replacedInput.GetInput());
+			}
+			int total = 0;
+			foreach (string piece in split) {
+				int value = ParseNumber(piece);
+				if (value < 0) throw new ArgumentException("negatives not allowed");
+				if (value < 999) {
+					total += value;
+				}
+			}
+			return total;
+		}
+
+		private int ParseNumber(string input) {
+			try {
+				return int.Parse(input);
+			} catch (FormatException) {
+				return 0;
+			}
+		}
+
+		private bool HasOneArgument(string[] split) {
+			return split.Length == 0;
+		}
+
+		private class NumberedString {
+			private const string Slashes = "//";
+			private const int IndexAfterSlashes = 2;
+
+			private readonly string delimiter = ",";
+			private readonly string input;
+
+			public NumberedString(string input) {
+				this.input = input;
+				if (input.StartsWith(Slashes)) {
+					delimiter = input.Substring(IndexAfterSlashes, getFirstNewLine(input));
+					this.input = input.Substring(getFirstNewLine(input));
+				}
+			}
+
+			private NumberedString(string input, string delimiter) {
+				this.input = input;
+				this.delimiter = delimiter;
+			}
+
+			private int getFirstNewLine(string value) {
+				return value.IndexOf('\n') - 1;
+			}
+
+			public NumberedString ReturnNewInstanceWithNewLinesReplacedByDelimiter() {
+				return new NumberedString(input.Replace("\n", delimiter), delimiter);
+			}
+
+			public string GetInput() {
+				return input;
+			}
+
+			public string[] GetNumbersAsString() {
+				return input.Split(delimiter.ToCharArray());
+			}
+		}
+	}
+}
---
+++ StringCalculatorTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Pyroxene.Kata.StringCalculator
+{
+	public class StringCalculatorTest
+	{
+		[Test]
+		public void ReturnsZeroWhenArgumentIsEmptyString() {
+			Assert.AreEqual(0, MakeStringCalculator().Add(""));
+		}
+
+		[Test]
+		public void ReturnsOneWhenArgumentIsOne() {
+			Assert.AreEqual(1, MakeStringCalculator().Add("1"));
+		}
+
+		[Test]
+		public void ReturnsSumOfTwoNumbersCommaSeparated() {
+			Assert.AreEqual(3, MakeStringCalculator().Add("1,2"));
+		}
+
+		[Test]
+		public void ReturnsSumOfMoreThanTwoNumbers() {
+			Assert.AreEqual(10, MakeStringCalculator().Add("1,2,3,4"));
+		}
+
+		[Test]
+		public void HandleNewLineAsDelimiters() {
+			Assert.AreEqual(6, MakeStringCalculator().Add("1\n,2,3"));
+		}
+
+		[Test]
+		public void HandleNewDelimiterGivenAfterTwoSlashes() {
+			Assert.AreEqual(6, MakeStringCalculator().Add("//;\n1;2;3"));
+		}
+
+		[Test, ExpectedException(typeof(ArgumentException))]
+		public void ShouldThrowException() {
+			MakeStringCalculator().Add("-1");
+		}
+
+		[Test]
+		public void IgnoreNumbersBiggerThan1000() {
+			Assert.AreEqual(3, MakeStringCalculator().Add("1,1000,2"));
+		}
+
+		// bugs:
+		// hard: new lines not working when using custom delimiter
+		[Test]
+		public void HandleNewDelimiterWithNewLines() {
+			Assert.AreEqual(60, MakeStringCalculator().Add("//;\n10;20\n30;\n"));
+		}
+
+		// easy: wrong input
+		[Test]
+		public void HandleNull() {
+			Assert.AreEqual(0, MakeStringCalculator().Add(null));
+		}
+
+		// edge case, 999 should still be counted
+		[Test]
+		public void ShouldCount999() {
+			Assert.AreEqual(1000, MakeStringCalculator().Add("1,999"));
+		}
+
+		private StringCalculator MakeStringCalculator() {
+			return new StringCalculator();
+		}
+	}
+}
